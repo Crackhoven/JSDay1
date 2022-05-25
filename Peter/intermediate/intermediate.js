7051849078f459bb553c4ee725dda35303c18179
Peter Aigner <mastapetz@icloud.com>
2022-05-25 11:53:50 +0200

    finish inter E2

--- Peter/intermediate/intermediate.js
+++ Peter/intermediate/intermediate.js
@@ -1,11 +1,17 @@
-// const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"];
-// console.log(cars);
-// console.log(cars.sort());
+const cars = ["Tesla", "Audi", "Renault", "Volvo", "Mazda", "Fiat", "Ferrari"]; //generate Array
+console.log(cars); //show contents of Array in console
+console.log(cars.sort()); //sorts contents of Array alphabetically
 /* l1-3 work */
 //Excercise 2 Array 1
 const fruits = ["apple", "banana", "kiwi"];
 console.log(fruits);
-fruits.push("orange");
+fruits.push("orange"); //adds element at end of Array
 console.log(fruits);
-fruits.pop();
+fruits.pop(); //removes last element of Array
 console.log(fruits);
+//Excercise 2 Array 2
+const animals = ["monkey", "horse", "dog"];
+console.log(animals);
+console.log(animals.sort());
+console.log(animals.unshift("cat")); //adds new element at the beginning of Array and returns new length in console
+console.log(animals);
