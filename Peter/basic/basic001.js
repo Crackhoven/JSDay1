cd55ab364170b5b1460a0c74d6bc5b8232d5ae62
Peter Aigner <mastapetz@icloud.com>
2022-05-25 10:42:25 +0200

    add array to external js

--- Peter/basic/basic001.js
+++ Peter/basic/basic001.js
@@ -1,3 +1,3 @@
 console.log("Hi my name is Martin");    //basic 2
-
-
+var names =["Martin", "Thomas", "Peter", "Mathias" , "Niki"];           //basic 4
+console.log("The most valuable player of this match is ", names[2]);    //basic 4
