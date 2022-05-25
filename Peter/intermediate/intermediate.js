43a44d8ce2ab7eb04156b50499e8a926ead3ee2a
Peter Aigner <mastapetz@icloud.com>
2022-05-25 11:43:11 +0200

    start excercise 2-1

--- Peter/intermediate/intermediate.js
+++ Peter/intermediate/intermediate.js
@@ -2,3 +2,10 @@
 // console.log(cars);
 // console.log(cars.sort());
 /* l1-3 work */
+//Excercise 2 Array 1
+const fruits = ["apple", "banana", "kiwi"];
+console.log(fruits);
+fruits.push("orange");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
