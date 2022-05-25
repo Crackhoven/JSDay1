f5ec06e5fe1f7c369bc535443f5d2ebff70d5805
Crackhoven <jan-fiedler@hotmail.com>
2022-05-25 13:45:16 +0200

    advanced done

---
+++ Jan/Advanced/advanced1.js
@@ -0,0 +1,27 @@
+var a = 7;
+
+var b = 14;
+
+var c = 21;
+
+var d = 36;
+
+var e = 42;
+
+var f = a + b + c + d + e;
+
+var g = 1;
+
+var h = 15;
+
+var i = 8;
+
+var j = 1;
+
+var k = g*h*i*j;
+
+var l = f/k;
+
+document.write(f + "<br>" + "<br>" + "<br>");
+document.write(k + "<br>" + "<br>" + "<br>");
+document.write(l);
