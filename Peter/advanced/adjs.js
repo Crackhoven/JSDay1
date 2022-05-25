9d05f6727fbdee2d5f172a3ef882a4cc6ed19eba
Peter Aigner <mastapetz@icloud.com>
2022-05-25 13:16:27 +0200

    finish exc 1

---
+++ Peter/advanced/adjs.js
@@ -0,0 +1,23 @@
+//Excercise 1
+//sum variables
+var a = 7;
+var b = 14;
+var c = "21";
+var d = "36";
+var e = 42;
+var sum_e1;
+sum_e1 = a + b + Number(c) + Number(d) + e;
+console.log(sum_e1);
+//multiply variables
+var f = "1";
+var g = 15;
+var h = 8;
+var i = "1";
+var mul_e1;
+mul_e1 = Number(f) * g * h * Number(i);
+console.log(mul_e1);
+//divide results
+document.write(sum_e1 / mul_e1);
+document.write("<hr>");
+//==============================
+//Excercise 2
